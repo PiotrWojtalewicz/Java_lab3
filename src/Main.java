9b57af1323711d9c8f2b579635e4dd088f1708fc
PiotrWojtalewicz <piowojt19@gmail.com>
2021-12-29 16:14:21 +0100

    zadanie 1.3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
@@ -6,14 +9,35 @@ public class Main {
 
         System.out.println( "zadanie 1a");
 
-        String[] liczby = {LiczbaEnum.Jeden.toString(), LiczbaEnum.Dwa.toString(),LiczbaEnum.Trzy.ToString()};
-        System.out.println("Liczby to " + liczby);
+        enum LiczbaEnum {
+            JEDEN, DWA, TRZY, CZTERY, PIĘĆ,SZEŚĆ;
+        }
+
+        System.out.println("Zadanie 1b");
+        enum StatusEnum{
+            KONTYNUUJEMY,KONIEC
+        }
+        System.out.println("Zadanie 1cd");
+        Scanner scan = new Scanner(System.in);
+
+        int i = scan.nextInt();
+        while(i > 0){
+            System.out.println("true");
+        i++;
+        }
+        switch (i){
+            case 0:
+                System.out.println(StatusEnum[1]);
+                break;
+                case 1;
+        }
+
+
+
         /* ćw 1 enum-y */
 
         /* zad.1
         a) Stworzyć prosty enum "LiczbyEnum" z liczbami od 1 do 6 zapisanymi słownie,
-
-
         b) stworzyć prosty enum "StatusEnum" z wartościami KONTYNUUJEMY i KONIEC,
         c) stworzyć pętlę while z wartością true w warunku (nieskończona pętla),
         d) niech w pętli są podawane wartości liczbowe z klawiatury,
