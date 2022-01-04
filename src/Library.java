d3657614b5bbe938ad629747c2cb8fdff8452cbc
PiotrWojtalewicz <piowojt19@gmail.com>
2022-01-04 11:39:39 +0100

    Zadanie4.4

---
+++ src/Library.java
@@ -0,0 +1,12 @@
+public class Library {
+    protected String subject;
+    protected String title;
+    protected int quantity;
+
+    Library(){
+        this.subject = subject;
+        this.title = title;
+        this.quantity = quantity;
+    }
+}
+
