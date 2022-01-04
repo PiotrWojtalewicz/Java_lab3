525845af03f63dcde824257b071e037252c1e1da
PiotrWojtalewicz <piowojt19@gmail.com>
2022-01-04 11:45:11 +0100

    Zadanie 5.1

--- src/Library.java
+++ src/Library.java
@@ -2,11 +2,22 @@ public class Library {
     protected String subject;
     protected String title;
     protected int quantity;
+    protected boolean recomende;
 
-    Library(){
+
+    public Library() {
+        this.subject = "";
+        this.title = "";
+        this.quantity = 0;
+        this.recomende = true;
+    }
+
+    public Library(String subject, String title, int quantity) {
         this.subject = subject;
         this.title = title;
         this.quantity = quantity;
+        recomende = true;
     }
-}
+    }
+
 
