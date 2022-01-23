3f30c70a03b334e08b09c927f6388c48425beaa4
PiotrWojtalewicz <piowojt19@gmail.com>
2022-01-23 19:12:04 +0100

    Zadanie 5.10- ustawienie Setterów

--- src/Osoba.java
+++ src/Osoba.java
@@ -7,19 +7,19 @@ public class Osoba {
     private int income;
 
 
-public Osoba(String name, String surname, int age) {
-            this.name = "";
-            this.surname = "";
-            this.age = 0;
-            this.income = 0;
-        }
+    public Osoba(String name, String surname, int age) {
+        this.name = "";
+        this.surname = "";
+        this.age = 0;
+        this.income = 0;
+    }
 
     public Osoba(String name, String surname, int age, int income) {
-            this.name = name;
-            this.surname = surname;
-            this.age = age;
-            this.income = income;
-        }
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.income = income;
+    }
 
     /* Metoda toString() za pomocą której będziemy wyświetlać zawartość klas */
     @Override
@@ -32,5 +32,18 @@ public Osoba(String name, String surname, int age) {
                 ", income= " + income +
                 '}';
     }
+
+    public void setName(String x) {
+        this.name = x;
     }
+    public void setSurname(String y) {
+        this.surname = y;
+    }
+    public void setAge(int z) {
+        this.age = z;
+    }
+    public void setIncome(int a) {
+        this.income = a;
+    }
+}
 
